a2329e6777a5dfe7d5afa1f9cfc2d9e0c13ecc68
Elizabeth Sanger <libsang@gmail.com>
2016-09-13 11:42:32 -0500

    added firebase config

--- app/app.js
+++ app/app.js
@@ -1,1 +1,23 @@
-console.log('app.js connected')
+"use strict";
+console.log('app.js connected');
+
+var app = angular.module("ChoreApp", ["ngRoute"])
+
+.constant('FirebaseURL', 'https://its-chore-turn.firebaseio.com');
+
+app.config(function($routeProvider){
+    $routeProvider.
+        when("/", {
+            templateUrl: '/partials/flower-arrangements.html',
+            controller: "flowerCtrl"
+        });
+    });
+
+app.run( ($location, FBCreds) => {
+    let creds = FBCreds;
+    let authConfig = {
+        apiKey: creds.key,
+        authDomain: creds.authDomain
+    };
+    firebase.initializeApp(authConfig);
+});
