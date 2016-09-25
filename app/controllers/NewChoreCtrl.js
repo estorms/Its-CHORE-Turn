48bb0b355feb0486731df6a550f582339bdb45df
Elizabeth Sanger <libsang@gmail.com>
2016-09-25 10:03:03 -0500

    added datepicker for chore due date

--- app/controllers/NewChoreCtrl.js
+++ app/controllers/NewChoreCtrl.js
@@ -32,7 +32,7 @@ $scope.btnText = "Save That Nasty Chore!";
 $scope.newChore = {
     name: '',
     description: '',
-    dueDate: '', //when pushing to FB, was registering a blank string until you put in ng-model to the partial, now it's not registering at all as a key on newChore
+    //when pushing to FB, was registering a blank string until you put in ng-model to the partial, now it's not registering at all as a key on newChore
     assignedMember: '',
     // completed: false
     completed: false,
@@ -44,6 +44,40 @@ $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 3 // Creates a dropdown of 3 years to control year
   });
+
+
+
+var currentTime = new Date();
+$scope.currentTime = currentTime;
+$scope.month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+$scope.monthShort = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+$scope.weekdaysFull = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+$scope.weekdaysLetter = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
+$scope.disable = [false, 1, 7];
+$scope.today = 'Today';
+$scope.clear = 'Clear';
+$scope.close = 'Close';
+var days = 15;
+$scope.minDate = (new Date($scope.currentTime.getTime() - ( 1000 * 60 * 60 *24 * days ))).toISOString();
+$scope.maxDate = (new Date($scope.currentTime.getTime() + ( 1000 * 60 * 60 *24 * days ))).toISOString();
+$scope.onStart = function () {
+    console.log('onStart');
+};
+$scope.onRender = function () {
+    console.log('onRender');
+};
+$scope.onOpen = function () {
+    console.log('onOpen');
+};
+$scope.onClose = function () {
+    console.log('onClose');
+};
+$scope.onSet = function () {
+    console.log('onSet');
+};
+$scope.onStop = function () {
+    console.log('onStop');
+};
 
 let accesshousehold = () => {
     console.log('you are inside accesshousehold, this is the first result, a user ID:', hId);
