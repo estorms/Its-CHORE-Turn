f0934399f924e48bab74108521274363923a916f
Elizabeth Sanger <libsang@gmail.com>
2016-09-25 14:07:28 -0500

    template bar chart established

--- app/controllers/HouseholdDataCtrl.js
+++ app/controllers/HouseholdDataCtrl.js
@@ -1,5 +1,12 @@
 app.controller("HouseholdDataCtrl", function ($scope, ChoreFactory, $routeParams, $window, $location) {
     // $scope.bgimg = "http://img.wikinut.com/img/19hgv38l3mly4kn3/jpeg/0/Happy-Couple.jpeg";
+ $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  $scope.series = ['Series A', 'Series B'];
+
+  $scope.data = [
+    [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90]
+  ];
 
 let hId;
 let houseID;
